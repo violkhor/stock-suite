5edb63e20248d87c131cacd3e6e01f89ec0288ce
violkhor <alexkhor100@gmail.com>
2014-06-14 22:13:49 -0400

    user login from database

--- src/com/stock/service/CompanyParser.java
+++ src/com/stock/service/CompanyParser.java
@@ -1,11 +1,9 @@
 package com.stock.service;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
@@ -18,64 +16,56 @@ import com.stock.model.CompanyModel;
 
 @Service("companyParser")
 public class CompanyParser {
-	private SAXParserFactory factory;
-	private SAXParser saxParser;
-	private DefaultHandler handler;
-	private List<CompanyModel> companyList;
+    private SAXParserFactory factory;
+    private SAXParser saxParser;
+    private DefaultHandler handler;
+    private List<CompanyModel> companyList;
 
-	public CompanyParser() {
-		companyList = new ArrayList<CompanyModel>();
-		factory = SAXParserFactory.newInstance();
-		try {
-			File file = new File(
-					"E:/devprogs/eclipse-workspace/spring_recipes/stock-suite/resources/companyList.xml");
-			saxParser = factory.newSAXParser();
-			handler = new DefaultHandler() {
+    public CompanyParser() {
+        companyList = new ArrayList<CompanyModel>();
+        factory = SAXParserFactory.newInstance();
+        try {
+            File file = new File("companyList.xml");
+            saxParser = factory.newSAXParser();
+            handler = new DefaultHandler() {
+                //
+                boolean industry;
+                boolean company;
 
-				//
-				boolean industry;
-				boolean company;
+                public void startElement(String uri, String localName, String qName,
+                        Attributes attributes) throws SAXException {
+                    if (qName.equalsIgnoreCase("industry"))
+                        industry = true;
+                    else if (qName.equalsIgnoreCase("company")) {
+                        company = true;
+                        CompanyModel cm = new CompanyModel();
+                        cm.setName(attributes.getValue(0));
+                        cm.setSymbol(attributes.getValue(1));
+                        if (!companyList.contains(cm)) companyList.add(cm);
+                    }
+                }
 
-				public void startElement(String uri, String localName,
-						String qName, Attributes attributes)
-						throws SAXException {
+                public void endElement(String uri, String localName, String qName)
+                        throws SAXException {
+                }
 
-					if (qName.equalsIgnoreCase("industry"))
-						industry = true;
-					else if (qName.equalsIgnoreCase("company")) {
-						company = true;
-						CompanyModel cm = new CompanyModel();
-						cm.setName(attributes.getValue(0));
-						cm.setSymbol(attributes.getValue(1));
-						if (!companyList.contains(cm))
-							companyList.add(cm);
-					}
-				}
+                public void characters(char ch[], int start, int length)
+                        throws SAXException {
+                    if (industry) {
+                        industry = false;
+                    }
+                    if (company) company = false;
+                }
+            };
+            saxParser.parse(file, handler);
+        }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-				public void endElement(String uri, String localName,
-						String qName) throws SAXException {
-
-				}
-
-				public void characters(char ch[], int start, int length)
-						throws SAXException {
-					if (industry) {
-						industry = false;
-					}
-					if (company)
-						company = false;
-				}
-			};
-			saxParser.parse(file, handler);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	public List<CompanyModel> getCompanyList() {
-		return companyList;
-	}
-
+    public List<CompanyModel> getCompanyList() {
+        return companyList;
+    }
 }
